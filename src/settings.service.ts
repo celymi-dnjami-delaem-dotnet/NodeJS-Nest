d125ee55d2c3faeddb308aca761850f9046d0b48
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-07-27 21:26:48 +0300

    Update settings service with ConfigService

--- src/settings.service.ts
+++ src/settings.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SettingsService {
     private readonly appPort: number;
 
-    constructor() {
-        this.appPort = process.env.APPLICATION_PORT ? Number(process.env.APPLICATION_PORT) : 3000;
+    constructor(private conf: ConfigService) {
+        this.appPort = conf.get<number>('APPLICATION_PORT', 3000);
     }
 
     getAppPort(): number {
