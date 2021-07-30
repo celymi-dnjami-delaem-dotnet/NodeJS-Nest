84b4dad025529bd16016d14178e04f90af57196f
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-07-30 22:58:29 +0300

    Add DTO's fields configuration

--- src/api/dto/actions/create-category.dto.ts
+++ src/api/dto/actions/create-category.dto.ts
@@ -1,3 +1,6 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateCategoryDto {
+    @IsNotEmpty()
     displayName: string;
 }
--- src/configuration/pipes.configuration.ts
+++ src/configuration/pipes.configuration.ts
@@ -1,5 +1,5 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 export default function registerPipes(app: INestApplication): void {
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 }
