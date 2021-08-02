19ee6eb1099f0d38f56e1a47408cb108084fea19
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-08-02 20:03:21 +0300

    Update Swagger controllers and endpoints description, add tags for separating endpoints

--- src/api/controllers/product.controller.ts
+++ src/api/controllers/product.controller.ts
@@ -1,40 +1,43 @@
-import { ApiResponse } from '@nestjs/swagger';
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateProductDto } from '../dto/actions/create-product.dto';
 import { ProductDto } from '../dto/models/product.dto';
 import { ProductService } from '../../bl/services/product.service';
 
+@ApiTags('Products')
 @Controller('api/product')
 export class ProductController {
     constructor(private readonly productService: ProductService) {}
 
     @Get('id/:id')
-    @ApiResponse({ type: ProductDto })
+    @ApiOkResponse({ type: ProductDto, description: 'OK' })
+    @ApiNotFoundResponse({ description: 'Not Found' })
     async getProducts(@Param('id') id: string): Promise<ProductDto> {
         return this.productService.getProductById(id);
     }
 
     @Post()
-    @HttpCode(HttpStatus.CREATED)
-    @ApiResponse({ type: ProductDto })
+    @ApiCreatedResponse({ type: ProductDto, description: 'Created' })
     async createProduct(@Body() productDto: CreateProductDto): Promise<ProductDto> {
         return this.productService.createProduct(productDto);
     }
 
     @Put()
-    @ApiResponse({ type: ProductDto })
+    @ApiOkResponse({ type: ProductDto, description: 'OK' })
     async updateCategory(@Body() category: ProductDto): Promise<ProductDto> {
         return await this.productService.updateProduct(category);
     }
 
-    @Delete('soft-remove/id/:id')
-    @HttpCode(HttpStatus.NO_CONTENT)
+    @Delete('id/:id')
+    @ApiNoContentResponse({ description: 'No Content' })
+    @ApiNotFoundResponse({ description: 'Not Found' })
     async softRemoveCategory(@Param('id') id: string): Promise<void> {
         await this.productService.softRemoveProduct(id);
     }
 
     @Delete('id/:id')
-    @HttpCode(HttpStatus.NO_CONTENT)
+    @ApiNoContentResponse({ description: 'No Content' })
+    @ApiNotFoundResponse({ description: 'Not Found' })
     async removeCategory(@Param('id') id: string): Promise<void> {
         await this.productService.removeProduct(id);
     }
