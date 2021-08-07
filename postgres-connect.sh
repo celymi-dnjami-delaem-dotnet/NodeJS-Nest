5bd655aacd1a39c5ff2fc493e8bde14c7cc68303
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-08-07 17:30:47 +0300

    Require username in PostgreSQL connection script

--- postgres-connect.sh
+++ postgres-connect.sh
@@ -1,4 +1,7 @@
+echo "Please, enter user name: "
+read userName
+
 echo "Please, enter database name: "
 read dbName
 
-docker exec -it $(docker ps -aqf "name=postgres_db") psql -U root -d ${dbName}
+docker exec -it $(docker ps -aqf "name=postgres_db") psql -U ${userName} -d ${dbName}
