be3976b1d3dacba4f7b069f235b93d4e8f95228c
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-08-08 10:31:48 +0300

    Add Logging interceptor to ApiModule

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,12 +1,21 @@
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { BlModule } from '../bl/bl.module';
 import { CategoryController } from './controllers/category.controller';
+import { ConsoleLogger, Module } from '@nestjs/common';
 import { HealthCheckController } from './controllers/health-check.controller';
-import { Module } from '@nestjs/common';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 import { ProductController } from './controllers/product.controller';
 import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
     imports: [BlModule, TerminusModule],
     controllers: [ProductController, CategoryController, HealthCheckController],
+    providers: [
+        ConsoleLogger,
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: LoggingInterceptor,
+        },
+    ],
 })
 export class ApiModule {}
